22061bac272fb59100affe06e2f137b40bb49a44
Giovani Meza <giovanidav5@outlook.com>
2022-07-02 22:48:40 -0600

    General change
    Modifies links to website paths

--- frontend/src/app/components/PiePagina.tsx
+++ frontend/src/app/components/PiePagina.tsx
@@ -1,27 +1,55 @@
-import React, { Fragment } from 'react';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function PiePagina() {
+interface props {
+	pie: string | undefined;
+}
+function PiePagina({ pie }: props) {
 	return (
-		<nav className="barra-navegacion">
-			<ul className="navegacion-pagina">
+		<nav
+			className="barra-navegacion-pie"
+			style={{
+				background: `${pie}`
+			}}
+		>
+			<ul className="navegacion-pie">
 				<li className="enlace-navegacion">
-					<Button className="btn">Pagina principal PPI</Button>
+					<Link className="" to={'/Nosotros'}>
+						Plataforma Psicometrica Integral
+					</Link>
 				</li>
 				<li className="enlace-navegacion">
-					<Button className="btn">Nuestros servicios</Button>
+					<Link className="" to={'/T&C'}>
+						Terminos y condiciones
+					</Link>
 				</li>
 				<li className="enlace-navegacion">
-					<Button className="btn">Inicio de candidatos</Button>
+					<Link className="" to={'/Privacidad'}>
+						Acuerdo de privacidad
+					</Link>
 				</li>
 			</ul>
 
-			<ul className="navegacion-cuenta">
+			<ul className="navegacion-pie">
 				<li className="enlace-navegacion">
-					<Button className="btn">Inicio de candidatos</Button>
+					<Link className="" to={'/Contacto'}>
+						Contacto
+					</Link>
 				</li>
 				<li className="enlace-navegacion">
-					<Button className="btn">Inicio de administradores</Button>
+					<Link className="" to={'/API'}>
+						Instagram
+					</Link>
+				</li>
+				<li className="enlace-navegacion">
+					<Link className="" to={'/API'}>
+						Facebook
+					</Link>
+				</li>
+				<li className="enlace-navegacion">
+					<Link className="" to={'/API'}>
+						copyright
+					</Link>
 				</li>
 			</ul>
 		</nav>
